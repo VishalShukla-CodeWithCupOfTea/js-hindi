fe21bf10e0db65d29b2bfeb93bae95e2b268854a
VishalShukla-CodeWithCupOfTea <52935712+VishalShukla-CodeWithCupOfTea@users.noreply.github.com>
2024-07-30 08:54:51 +0000

    setup done

---
+++ 01_basics/01_Variables.js
@@ -0,0 +1,13 @@
+const accountId = 1224456;
+let accountEmail = "shuklavishal004@gmail.com";
+var accountPassword = "12345"
+accountCity = "Jaunpur"
+
+
+//accountId = 233444
+accountEmail = "vs.kumar@gmail.com";
+accountPassword = "090909" 
+
+console.log(accountId);
+
+console.table([accountId, accountEmail, accountPassword, accountCity])

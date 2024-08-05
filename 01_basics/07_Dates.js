acdd875f0bd90db2c34edf5e01105ec502a69b49
Vishal_Shukla <shuklavishal004@gmail.com>
2024-08-05 18:16:13 +0530

    Array

---
+++ 01_basics/07_Dates.js
@@ -0,0 +1,31 @@
+
+ let myDate = new Date();
+//  console.log(`${myDate} simple date return by js.`);
+//  console.log(`${myDate.toString()} by toString() function.`);
+//  console.log(`${myDate.toISOString()} by toISOString() function.`);
+//  console.log(`${myDate.toJSON()} by toJSON() function.`);
+//  console.log(`${myDate.toLocaleDateString()} by toLocaleDateString() function.`);
+//  console.log(`${myDate.toLocaleTimeString()} by toLocaleTimeString() function.`);
+//  console.log(`${myDate.toUTCString()} by toUTCString() function.`);
+
+let dateOne = new Date(2024, 0, 23);
+//console.log(dateOne.toDateString());
+
+let dateTwo = new Date(2024, 0, 23, 5, 3);
+//console.log(dateTwo.toLocaleString());
+
+let dateThree = new Date("2024-01-12");
+//console.log(dateThree.toLocaleString());
+
+let dateFour = new Date("01-14-2024");
+//console.log(dateFour.toLocaleString());
+
+
+let myTimeStamp = Date.now();
+// console.log(myTimeStamp);
+// console.log(dateFour.getTime());
+//console.log(Math.floor(Date.now()/1000));
+
+let newDate = new Date();
+//console.log(newDate.getDay());
+

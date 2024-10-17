fa7d00c7568d6f130a4afc357ad1ba770f15c3e1
Vishal_Shukla <shuklavishal004@gmail.com>
2024-10-17 16:53:38 +0530

    iteration

--- 05_iteration/one.js
+++ 05_iteration/one.js
@@ -1,6 +1,11 @@
 // for
 
 for (let index = 0; index < 10; index++) {
-    const element = index;
-    console.log(element);
+    GetPrint(index);
+    // const element = index;
+    // console.log(element);
+}
+
+function GetPrint(index){
+    console.log(`${index} A`);
 }
